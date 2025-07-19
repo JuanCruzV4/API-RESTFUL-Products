09ed976021a3619f25ee3e00c9e40283aaa61623
JuanCruzV4 <juancruz.vazquez87@hotmail.com>
2025-07-18 23:42:20 -0300

    Authentication

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,8 +3,7 @@ import jwt from 'jsonwebtoken';
 export const auth = (req, res, next) => {
     const token = req.headers["authorization"]?.split(" ")[1];
 
-    console.log(token)
-    // if (!token) return res.sendStatus(401);
+    if (!token) return res.sendStatus(401);
 
     jwt.verify(token, process.env.JWT_SECRET, (err) => {
         if (err) return res.sendStatus(403);
